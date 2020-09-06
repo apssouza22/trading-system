f52d0ce7609cd22bf95eeb7e2f30a37628e313a9
Alexsandro Souza <alexsandro.souza@deem.com>
2020-09-06 15:03:57 +0100

    Changed to Java 14

--- src/main/java/com/apssouza/mytrade/trading/misc/helper/config/TradingParams.java
+++ src/main/java/com/apssouza/mytrade/trading/misc/helper/config/TradingParams.java
@@ -2,14 +2,10 @@ package com.apssouza.mytrade.trading.misc.helper.config;
 
 import com.apssouza.mytrade.trading.forex.session.SessionType;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 public class TradingParams {
     public static String brokerHost;
