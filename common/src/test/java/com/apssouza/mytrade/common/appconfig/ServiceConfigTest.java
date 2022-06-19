01004aa6637d1d5e0d94f58d7334b5ad1ec1669c
ales-kunst <ales.kunst@gmail.com>
2022-06-19 09:46:01 +0200

    Fix ServiceConfigTest

--- common/src/test/java/com/apssouza/mytrade/common/appconfig/ServiceConfigTest.java
+++ common/src/test/java/com/apssouza/mytrade/common/appconfig/ServiceConfigTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 
@@ -14,7 +15,7 @@ public class ServiceConfigTest {
     @Test
     public void test_load() throws URISyntaxException {
         URI uri = ServiceConfigTest.class.getProtectionDomain().getCodeSource().getLocation().toURI();
-        ConfigurationProvider provider = ServiceConfig.load(uri.getPath(), "test");
+        ConfigurationProvider provider = ServiceConfig.load(Paths.get(uri).toString(), "test");
         Properties properties = provider.allConfigurationAsProperties();
         assertEquals(50051, properties.get("grpc.server.port"));
     }
